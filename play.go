dd2aba0174b9138e956f47d1b1dd17f2f3b5a725
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-03-19 22:38:17 -0500

    add min function

--- play.go
+++ play.go
@@ -12,4 +12,19 @@ func main() {
 	if attended["Ann"] { // will be false if person is not in the map
 		fmt.Println("Ann", "was at the meeting")
 	}
+	values := []int{1, 4, 5, 6, 7, 8, 1, 6, 2, 6, 7, 1, 11111}
+	fmt.Println(min(values...))
+
+	fmt.Println(^uint(0))
+	fmt.Println(^uint(0) >> 1)
+}
+
+func min(a ...int) int {
+	min := int(^uint(0) >> 1) // largest int
+	for _, i := range a {
+		if i < min {
+			min = i
+		}
+	}
+	return min
 }

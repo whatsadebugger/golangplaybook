9d608c428703ef4d0a20d92b18272c6b7254e44b
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-03-20 21:45:41 -0500

    qr generator from effective go

--- play.go
+++ play.go
@@ -1,43 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"github.com/tidwall/gjson"
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
 )
 
-type winner interface {
-	win()
-}
+var addr = flag.String("addr", ":1718", "http service address") // Q=17, R=18
+
+var templ = template.Must(template.New("qr").Parse(templateStr))
 
 func main() {
-
-	attended := map[string]bool{
-		"Ann": true,
-		"Joe": true,
+	flag.Parse()
+	http.Handle("/", http.HandlerFunc(QR))
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
 	}
-
-	if attended["Ann"] { // will be false if person is not in the map
-		fmt.Println("Ann", "was at the meeting")
-	}
-	values := []int{1, 4, 5, 6, 7, 8, 1, 6, 2, 6, 7, 1, 11111}
-	fmt.Println(min(values...))
-
-	fmt.Println(^uint(0))
-	fmt.Println(^uint(0) >> 1)
-
-	fmt.Println(gjson.Valid(``))
-	fmt.Println(gjson.Valid(`{}`))
-	fmt.Println(gjson.Valid(`{"true":1}`))
-	fmt.Println(gjson.Valid(`{"false":1,}`))
-
 }
 
-func min(a ...int) int {
-	min := int(^uint(0) >> 1) // largest int
-	for _, i := range a {
-		if i < min {
-			min = i
-		}
-	}
-	return min
+func QR(w http.ResponseWriter, req *http.Request) {
+	templ.Execute(w, req.FormValue("s"))
 }
+
+const templateStr = `
+<html>
+<head>
+<title>QR Link Generator</title>
+</head>
+<body>
+{{if .}}
+<img src="http://chart.apis.google.com/chart?chs=300x300&cht=qr&choe=UTF-8&chl={{.}}" />
+<br>
+{{.}}
+<br>
+<br>
+{{end}}
+<form action="/" name=f method="GET"><input maxLength=1024 size=70
+name=s value="" title="Text to QR Encode"><input type=submit
+value="Show QR" name=qr>
+</form>
+</body>
+</html>
+`

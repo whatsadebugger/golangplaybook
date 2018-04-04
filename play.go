5594626026996623b7ec138118a0d607e9cb88f6
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-04-04 18:17:30 -0500

    use of atomic values in go

--- play.go
+++ play.go
@@ -1,41 +1,40 @@
 package main
 
 import "fmt"
-
-type notifier interface {
-	notify()
-}
-
-type user struct {
-	name  string
-	email string
-}
-
-type admin struct {
-	user
-	level string
-}
-
-func (a *admin) notify() {
-	fmt.Printf("Sending email to admin: %s<%s>\n", a.name, a.email)
-}
-
-func (u *user) notify() {
-	fmt.Printf("Sending email to user: %s<%s>\n", u.name, u.email)
-}
+import "time"
+import "sync/atomic"
 
 func main() {
-	u := user{name: "ahmad tabbakha", email: "tabba.ahmad@gmail.com"}
-	ad := admin{
-		user:  user{name: "ahmad leet", email: "ahmad1337@admin.com"},
-		level: "super",
-	}
-	sendNotification(&u)
-	sendNotification(&u)
-	sendNotification(&ad)
-	sendNotification(&ad)
-}
 
-func sendNotification(n notifier) {
-	n.notify()
+	var ops uint64
+
+	// To simulate concurrent updates, we'll start 50
+	// goroutines that each increment the counter about
+	// once a millisecond.
+	for i := 0; i < 50; i++ {
+		go func() {
+			for {
+				// To atomically increment the counter we
+				// use `AddUint64`, giving it the memory
+				// address of our `ops` counter with the
+				// `&` syntax.
+				atomic.AddUint64(&ops, 1)
+
+				// Wait a bit between increments.
+				time.Sleep(time.Millisecond)
+			}
+		}()
+	}
+
+	// Wait a second to allow some ops to accumulate.
+	time.Sleep(time.Second)
+
+	// In order to safely use the counter while it's still
+	// being updated by other goroutines, we extract a
+	// copy of the current value into `opsFinal` via
+	// `LoadUint64`. As above we need to give this
+	// function the memory address `&ops` from which to
+	// fetch the value.
+	opsFinal := atomic.LoadUint64(&ops)
+	fmt.Println("ops:", opsFinal)
 }

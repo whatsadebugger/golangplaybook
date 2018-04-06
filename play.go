4bdda83623f4d280a1593f95e76c0370a2b883b7
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-04-05 19:37:59 -0500

    gobyexample.com/json example to learn about encoding/json

--- play.go
+++ play.go
@@ -1,40 +1,112 @@
+// Go offers built-in support for JSON encoding and
+// decoding, including to and from built-in and custom
+// data types.
+
 package main
 
+import "encoding/json"
 import "fmt"
-import "time"
-import "sync/atomic"
+import "os"
+
+// We'll use these two structs to demonstrate encoding and
+// decoding of custom types below.
+type response1 struct {
+	Page   int
+	Fruits []string
+}
+type response2 struct {
+	Page   int      `json:"page"`
+	Fruits []string `json:"fruits"`
+}
 
 func main() {
 
-	var ops uint64
+	// First we'll look at encoding basic data types to
+	// JSON strings. Here are some examples for atomic
+	// values.
+	bolB, _ := json.Marshal(true)
+	fmt.Println(string(bolB))
 
-	// To simulate concurrent updates, we'll start 50
-	// goroutines that each increment the counter about
-	// once a millisecond.
-	for i := 0; i < 50; i++ {
-		go func() {
-			for {
-				// To atomically increment the counter we
-				// use `AddUint64`, giving it the memory
-				// address of our `ops` counter with the
-				// `&` syntax.
-				atomic.AddUint64(&ops, 1)
+	intB, _ := json.Marshal(1)
+	fmt.Println(string(intB))
 
-				// Wait a bit between increments.
-				time.Sleep(time.Millisecond)
-			}
-		}()
+	fltB, _ := json.Marshal(2.34)
+	fmt.Println(string(fltB))
+
+	strB, _ := json.Marshal("gopher")
+	fmt.Println(string(strB))
+
+	// And here are some for slices and maps, which encode
+	// to JSON arrays and objects as you'd expect.
+	slcD := []string{"apple", "peach", "pear"}
+	slcB, _ := json.Marshal(slcD)
+	fmt.Println(string(slcB))
+
+	mapD := map[string]int{"apple": 5, "lettuce": 7}
+	mapB, _ := json.Marshal(mapD)
+	fmt.Println(string(mapB))
+
+	// The JSON package can automatically encode your
+	// custom data types. It will only include exported
+	// fields in the encoded output and will by default
+	// use those names as the JSON keys.
+	res1D := &response1{
+		Page:   1,
+		Fruits: []string{"apple", "peach", "pear"}}
+	res1B, _ := json.Marshal(res1D)
+	fmt.Println(string(res1B))
+
+	// You can use tags on struct field declarations
+	// to customize the encoded JSON key names. Check the
+	// definition of `response2` above to see an example
+	// of such tags.
+	res2D := &response2{
+		Page:   1,
+		Fruits: []string{"apple", "peach", "pear"}}
+	res2B, _ := json.Marshal(res2D)
+	fmt.Println(string(res2B))
+
+	byt := []byte(`{"num":6.13,"strs":["a","b"]}`)
+
+	var dat map[string]interface{}
+
+	// Here's the actual decoding, and a check for
+	// associated errors.
+	if err := json.Unmarshal(byt, &dat); err != nil {
+		panic(err)
 	}
+	fmt.Println(dat)
 
-	// Wait a second to allow some ops to accumulate.
-	time.Sleep(time.Second)
+	// In order to use the values in the decoded map,
+	// we'll need to cast them to their appropriate type.
+	// For example here we cast the value in `num` to
+	// the expected `float64` type.
+	num := dat["num"].(float64)
+	fmt.Println(num)
 
-	// In order to safely use the counter while it's still
-	// being updated by other goroutines, we extract a
-	// copy of the current value into `opsFinal` via
-	// `LoadUint64`. As above we need to give this
-	// function the memory address `&ops` from which to
-	// fetch the value.
-	opsFinal := atomic.LoadUint64(&ops)
-	fmt.Println("ops:", opsFinal)
+	// Accessing nested data requires a series of
+	// casts.
+	strs := dat["strs"].([]interface{})
+	str1 := strs[0].(string)
+	fmt.Println(str1)
+
+	// We can also decode JSON into custom data types.
+	// This has the advantages of adding additional
+	// type-safety to our programs and eliminating the
+	// need for type assertions when accessing the decoded
+	// data.
+	str := `{"page": 1, "fruits": ["apple", "peach"]}`
+	res := response2{}
+	json.Unmarshal([]byte(str), &res)
+	fmt.Println(res)
+	fmt.Println(res.Fruits[0])
+
+	// In the examples above we always used bytes and
+	// strings as intermediates between the data and
+	// JSON representation on standard out. We can also
+	// stream JSON encodings directly to `os.Writer`s like
+	// `os.Stdout` or even HTTP response bodies.
+	enc := json.NewEncoder(os.Stdout)
+	d := map[string]int{"apple": 5, "lettuce": 7}
+	enc.Encode(d)
 }

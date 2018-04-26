fd31ec9e262d6e13faa49c77fff331efac37a9d7
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-04-25 21:02:38 -0500

    learning how to use reflection package. Starting with a basic use of reflect.TypeOf(x)

--- play.go
+++ play.go
@@ -1,33 +1,11 @@
 package main
 
-import "fmt"
-
-type Describer interface {
-	Describe()
-}
-type Person struct {
-	name string
-	age  int
-}
-
-func (p Person) Describe() {
-	fmt.Printf("%s is %d years old", p.name, p.age)
-}
-
-func findType(i interface{}) {
-	switch v := i.(type) {
-	case Describer:
-		v.Describe()
-	default:
-		fmt.Printf("unknown type\n")
-	}
-}
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	findType("Ahmad")
-	p := Person{
-		name: "Ahmad Tabbakha",
-		age:  24,
-	}
-	findType(p)
+	x := 3
+	fmt.Println("type:", reflect.TypeOf(x))
 }

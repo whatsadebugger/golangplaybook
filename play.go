af16f76d12919f6415565abbd87ad1c4c83c836a
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-05-16 18:47:59 -0500

    example to show the how to print a string version of any type

--- play.go
+++ play.go
@@ -1,31 +1,37 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	// "reflect"
+	"strconv"
 )
 
 func main() {
-	b := []byte(`{"Name":"Wednesday","Age":6,"Parents":["Gomez","Morticia"]}`)
-	var f interface{}
-	if err := json.Unmarshal(b, &f); err != nil {
-		panic(err)
-	}
-	m := f.(map[string]interface{})
+	var i interface{} = "hello"
 
-	for k, v := range m {
-		switch vv := v.(type) {
-		case string:
-			fmt.Println(k, "is string", vv)
-		case float64:
-			fmt.Println(k, "is float64", vv)
-		case []interface{}:
-			fmt.Println(k, "is an array:")
-			for i, u := range vv {
-				fmt.Println(i, u)
-			}
-		default:
-			fmt.Println(k, "is of a type I don't know how to handle")
+	s := i.(string)
+	fmt.Println(Sprint(s))
+}
+
+func Sprint(x interface{}) string {
+	type stringer interface {
+		String() string
+	}
+	switch x := x.(type) {
+	case stringer:
+		return x.String()
+	case string:
+		return x
+	case int:
+		return strconv.Itoa(x)
+	// ...similar cases for int16, uint32, and so on...
+	case bool:
+		if x {
+			return "true"
 		}
+		return "false"
+	default:
+		// array, chan, func, map, pointer, slice, struct
+		return "???"
 	}
 }

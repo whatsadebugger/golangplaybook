8c6c74558cd32727162f898f04dac2d5dd517eb7
Tabbakha, Ahmad <Ahmad.Tabbakha@tylertech.com>
2018-07-10 12:32:31 -0500

    move some files and fix comments

--- Channels/channel.go
+++ Channels/channel.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	// https://blog.mergermarket.it/now-youre-thinking-with-channels/ amazing read
-	intChan := make(chan int) // unbuffered channel blocks causes block
-	go sendOnThisChannel(intChan)
+	intChan := make(chan int) // unbuffered channel
+	go sendOnThisChannel(intChan) // if you take out the go here there will be a deadlock
 	number := <-intChan
 	fmt.Println("Got int ", number)
 }

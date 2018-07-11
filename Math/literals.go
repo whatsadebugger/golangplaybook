110556de127f33863e5725b939c42ebe611e4966
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-07-11 18:39:51 -0500

    add naz examples

--- Math/literals.go
+++ Math/literals.go
@@ -6,18 +6,14 @@ func main() {
 
 	fmt.Println("%d")
 	fmt.Printf("%d\n\n", 0xBadFace)
-	
+
 	fmt.Println("%x")
 	fmt.Printf("%x\n\n", 0xBadFace)
-	
+
 	fmt.Println("%E")
-	fmt.Printf("%E\n", .1E17)
-	fmt.Printf("%E\n", 6.67428E-11)
-	fmt.Printf("%E\n\n", 1.e+0)
+	fmt.Printf("%E\n", .1E-17)
 
 	fmt.Println("%f with modified precision")
-	fmt.Printf("%.0f\n", .1E17)
-	fmt.Printf("%.17f\n", 6.67428E-11)
-	fmt.Printf("%.17f\n", 1.e+0)
-}
+	fmt.Printf("%.18f\n", .1E-17)
 
+}

1c6ce5a470fd72780d77f15187afa8d6dda2ddc2
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-07-19 20:13:10 -0500

    show the types of literals

--- Math/literals.go
+++ Math/literals.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 
+	fmt.Println("reflect.TypeOf")
+	fmt.Println(reflect.TypeOf(0xBADFACE))
+	fmt.Println(reflect.TypeOf(.1E-17))
+	fmt.Println(reflect.TypeOf(0333))
+
+	fmt.Println()
+	fmt.Println()
+
+	fmt.Println("%d")
+	fmt.Printf("%d\n\n", 0xBadFace)
 	fmt.Println("%d")
 	fmt.Printf("%d\n\n", 0xBadFace)
 
@@ -11,7 +24,7 @@ func main() {
 	fmt.Printf("%x\n\n", 0xBadFace)
 
 	fmt.Println("%E")
-	fmt.Printf("%E\n", .1E-17)
+	fmt.Printf("%E\n\n", .1E-17)
 
 	fmt.Println("%f with modified precision")
 	fmt.Printf("%.18f\n", .1E-17)

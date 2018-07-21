4df41d40a12c6308dfdcacb0fce5e6ea0e30b060
tabbakha, ahmad <tabba.ahmad@gmail.com>
2018-07-21 13:12:21 -0500

    programmer day

--- Test/main.go
+++ Test/main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	divisibleSumPairs(6, 3, []int32{1, 3, 2, 6, 1, 2})
 	fmt.Println(migratoryBirds([]int32{1, 4, 4, 4, 5, 3}))
+	fmt.Println(solve(2000))
 }
 
 // Complete the divisibleSumPairs function below.
@@ -31,10 +32,25 @@ func migratoryBirds(ar []int32) int32 {
 	max := int32(-1)
 
 	for k, v := range count {
-		if v > max {
+		if v > max || (k < maxKey && v >= max) {
 			maxKey = k
 			max = v
 		}
 	}
 	return maxKey
+}
+
+// Complete the solve function below.
+func solve(year int32) string {
+	y := int(year)
+	progDay := ""
+	if y == 1918 {
+		progDay = fmt.Sprintf("%02d.%02d.%d", 26, 9, y)
+	} else if (y < 1918 && y%4 == 0) || (y > 1918 && ((y%400 == 0) || (y%4 == 0 && y%100 != 0))) {
+		progDay = fmt.Sprintf("%02d.%02d.%d", 12, 9, y)
+	} else {
+		progDay = fmt.Sprintf("%02d.%02d.%d", 13, 9, y)
+	}
+
+	return progDay
 }

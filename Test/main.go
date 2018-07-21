2d0222ef3097318d2e7df93ce6684db104c622a7
tabbakha, ahmad <tabba.ahmad@gmail.com>
2018-07-21 12:21:36 -0500

    migratory birds problem

--- Test/main.go
+++ Test/main.go
@@ -1,7 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
 	divisibleSumPairs(6, 3, []int32{1, 3, 2, 6, 1, 2})
+	fmt.Println(migratoryBirds([]int32{1, 4, 4, 4, 5, 3}))
 }
 
 // Complete the divisibleSumPairs function below.
@@ -15,4 +18,23 @@ func divisibleSumPairs(n int32, k int32, ar []int32) int32 {
 		}
 	}
 	return sumPairs
+}
+
+func migratoryBirds(ar []int32) int32 {
+	count := make(map[int32]int32)
+
+	for _, v := range ar {
+		count[v]++
+	}
+
+	maxKey := int32(-1)
+	max := int32(-1)
+
+	for k, v := range count {
+		if v > max {
+			maxKey = k
+			max = v
+		}
+	}
+	return maxKey
 }

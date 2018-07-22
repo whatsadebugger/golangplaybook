879d65a81bf2e63aca0adfebed145120c4168024
tabbakha, ahmad <tabba.ahmad@gmail.com>
2018-07-21 23:59:51 -0500

    Strings and bytes/unicode

--- Strings/strings.go
+++ Strings/strings.go
@@ -6,18 +6,27 @@ import (
 
 func main() {
 	const placeOfInterest = `⌘`
+	const sample = "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98"
 
-	fmt.Printf("plain string: ")
+	fmt.Printf("placeOfInterest string: ")
 	fmt.Printf("%s", placeOfInterest)
 	fmt.Printf("\n")
 
-	fmt.Printf("quoted string: ")
+	fmt.Printf("quoted placeOfInterest: ")
 	fmt.Printf("%+q", placeOfInterest)
 	fmt.Printf("\n")
 
 	fmt.Printf("hex bytes: ")
-	for i := 0; i < len(placeOfInterest); i++ {
-		fmt.Printf("%x ", placeOfInterest[i])
-	}
+	fmt.Printf("% X", placeOfInterest)
 	fmt.Printf("\n")
+
+	fmt.Printf("sample string: ")
+	fmt.Printf("%s", sample)
+	fmt.Printf("\n")
+
+	fmt.Printf("quoted placeOfInterest: ")
+	fmt.Printf("%+q", sample)
+	fmt.Printf("\n")
+
+	fmt.Printf("Hex values in sample: % X", sample)
 }

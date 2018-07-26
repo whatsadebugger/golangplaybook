d0a9e649454595585817efffa60712910d455f3a
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-07-26 06:14:39 -0500

    reverse string example

--- RandomCode/play.go
+++ RandomCode/play.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 func main() {
@@ -15,7 +16,28 @@ func main() {
 		fmt.Println(i)
 	}
 
-	divisibleSumPairs(6, 3, []int32{1, 3, 2, 6, 1, 2})
+	// divisibleSumPairs(6, 3, []int32{1, 3, 2, 6, 1, 2})
+
+	numbers := "123456789"
+	fmt.Println("type of an index in a string", reflect.TypeOf(numbers[0]))
+
+	print("numbers: ")
+	for _, v := range numbers {
+		print(v, " ")
+	}
+	println()
+
+	runes := []rune(numbers)
+	fmt.Println("runes intitial", string(runes))
+	size := len(runes)
+
+	for i, j := 0, size-1; i < size/2; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	fmt.Printf("runes rev: %+v\n", runes)
+	fmt.Println("string(runes):", string(runes))
+	fmt.Println("string(numbers):", string(numbers))
 }
 
 // Complete the divisibleSumPairs function below.

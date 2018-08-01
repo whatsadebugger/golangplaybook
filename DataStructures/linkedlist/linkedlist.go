6b1cd7ad6aa50a928e5947cf4d8a443cc2da9895
tabbakha, ahmad <tabba.ahmad@gmail.com>
2018-08-01 17:02:42 -0500

    Linked list WIP

---
+++ DataStructures/linkedlist/linkedlist.go
@@ -0,0 +1,23 @@
+package linkedlist
+
+// Node is a data structure which points to the next node in a linked list
+type Node struct {
+	Value interface{}
+	Next  *Node
+}
+
+// List is a linked list
+type List struct {
+}
+
+// NewList returns a new list
+func NewList() {}
+
+// Insert inserts item into given position
+func Insert() {}
+
+// Update updates item at given position
+func Update() {}
+
+// Delete deletes an item at given position
+func Delete() {}
---
+++ DataStructures/linkedlist/linkedlist_test.go
@@ -0,0 +1,9 @@
+package linkedlist
+
+import (
+	"testing"
+)
+
+func TestInsert(t *testing.T) {}
+func TestUpdate(t *testing.T) {}
+func TestDelete(t *testing.T) {}

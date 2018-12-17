555d5d806416be3fe3fb543fd8a2a89cb82afa58
Tabbakha, Ahmad <Ahmad.tabbakha@toyotaconnected.com>
2018-12-17 16:07:01 -0600

    json and tempates are awesome

--- JSON/createJSON.go
+++ JSON/createJSON.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
+	"text/template"
 )
 
 type testStructTags struct {
@@ -10,13 +12,32 @@ type testStructTags struct {
 }
 
 func main() {
-	JSONS := []string{`{"ahmad":1}`, `{"damha":5}`, `{"ahmad": 0, "damha":5}`, `{"damha": 0, "ahmad":5}`}
+	JSONS := []string{
+		`{"ahmad":1, "nest": {"wow":100,"look":"nohands","nested":{"key": 1337}}}`,
+		`{"Count":1, "wow":"wow"}`,
+		`{"damha":5}`,
+		`{"ahmad": 0, "damha":5}`,
+		`{"damha": 0, "ahmad":5}`,
+	}
 
 	for _, jason := range JSONS {
-		var tst testStructTags
+		var tst map[string]interface{}
 		if err := json.Unmarshal([]byte(jason), &tst); err != nil {
 			panic(err)
 		}
 		fmt.Println(tst)
+		data, _ := json.Marshal(tst)
+		fmt.Println(string(data))
 	}
+
+	jad := []byte(`{"Count":1, "Wow":"wow", "nest": {"key":1}}`)
+	var test map[string]interface{}
+	if err := json.Unmarshal(jad, &test); err != nil {
+		panic(err)
+	}
+	tmpl, err := template.New("test").Parse("{{.Count}} items are made of {{.Wow}} and nest {{.nest.key}}\n")
+	if err != nil {
+		panic(err)
+	}
+	tmpl.Execute(os.Stdout, test)
 }

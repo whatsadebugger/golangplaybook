6c5a3610359bc0719b4e68a3bc8f571994a9a25f
Ahmad Tabbakha <tabba.ahmad@gmail.com>
2024-10-25 15:08:19 -0500

    tz testing

--- days/time_test.go
+++ days/time_test.go
@@ -5,10 +5,17 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/whatsadebugger/golangplaybook/days"
 )
 
 func TestTomorrow(t *testing.T) {
 	tom := days.Tomorrow()
 	log.Println(tom.Format(time.RFC1123))
+}
+
+func TestTimeAssertion(t *testing.T) {
+	got := time.Unix(1729825337, 0).UTC().Format(time.RFC3339)
+	expected := "2024-10-25T03:02:17Z"
+	assert.Equal(t, expected, got)
 }
